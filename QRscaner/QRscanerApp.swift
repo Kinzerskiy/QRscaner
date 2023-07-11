48d6db46687c810a8faf24d4beb4445d960bf5a5
Kinzerskiy <kinzzersky@gmail.com>
2023-07-11 14:16:52 +0300

    Initial Commit

---
+++ QRscaner/QRscanerApp.swift
@@ -0,0 +1,17 @@
+//
+//  QRscanerApp.swift
+//  QRscaner
+//
+//  Created by Mac Pro on 11.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct QRscanerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
